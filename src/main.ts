9549a30d617f658264789e54cf61a3a671b77b75
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-05 09:52:34 +0200

    update cors allowed connection

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.use(
     cors({
       credentials: true,
-      origin: ['http://localhost:5173', ''],
+      origin: ['http://localhost:5173', 'https://carent-jade.vercel.app/'],
     }),
   );
   await app.listen(3000);
