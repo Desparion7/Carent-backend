23ac2fcd1023bd821a543b2c71317aff61594615
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-10 19:56:49 +0200

    add filters in cars service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -6,9 +6,12 @@ export class CarsController {
   constructor(private carsService: CarsService) {}
 
   @Get()
-  getCars(@Query() query: { brand: string }) {
+  getCars(
+    @Query() query: { brand: string; startDate: string; endDate: string },
+  ) {
     return this.carsService.getCars(query);
   }
+
   @Get(':name')
   getCarByName(@Param('name') name: string) {
     return this.carsService.getCarByName(name);
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -7,21 +7,43 @@ import { Car } from './interface/car.interface';
 export class CarsService {
   constructor(@InjectModel('Car') private carModel: Model<Car>) {}
 
-  async getCars({ brand }) {
-    if (brand === 'All') {
-      const cars = await this.carModel.find();
-      if (!cars) {
-        throw new NotFoundException('not found');
-      }
-      return cars;
-    } else {
-      const cars = await this.carModel.find({ brand });
-      if (!cars) {
-        throw new NotFoundException('not found');
-      }
+  async getCars({ brand, startDate, endDate }): Promise<Car[]> {
+    let cars = await this.carModel.find();
+    if (!cars) {
+      throw new NotFoundException('not found');
+    }
+    if (brand == 'All' && !startDate && !endDate) {
       return cars;
     }
+    if (brand !== 'All') {
+      cars = cars.filter((car) => car.brand === brand);
+    }
+    if (startDate && endDate) {
+      const convertedStartDate = new Date(startDate);
+      const convertedEndDate = new Date(endDate);
+
+      const dates = [];
+      // add dates beetwen start date and end date
+      while (convertedStartDate <= convertedEndDate) {
+        dates.push(new Date(convertedStartDate));
+        convertedStartDate.setDate(convertedStartDate.getDate() + 1);
+      }
+      // convert date object to string
+      const formattedDates = dates.map((date) => {
+        const dateObj = new Date(date);
+        return dateObj.toISOString();
+      });
+      // filter cars and left only these with free date
+      cars = cars.filter((car) => {
+        return !car.calendar.some((date) =>
+          formattedDates.includes(date.toISOString()),
+        );
+      });
+    }
+
+    return cars;
   }
+
   async getCarByName(name: string) {
     const car = await this.carModel.findOne({ name }).exec();
     if (!car) {
